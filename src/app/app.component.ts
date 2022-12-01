bee851d061d4b85373a767d1cfa78ec4a2906f67
Ruchakale19 <kalerucha19@gmail.com>
2022-12-01 11:07:08 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
+
 import { Component } from '@angular/core';
+import { lastValueFrom } from 'rxjs';
+import { GoogleApiService, UserInfo } from './google-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SampleForOAuth';
+
+  mailSnippets: string[] = []
+  userInfo?: UserInfo
+
+  constructor(private readonly googleApi: GoogleApiService) {
+    debugger;
+    googleApi.userProfileSubject.subscribe( info => {
+      this.userInfo = info
+    })
+    debugger;
+  }
+
+  isLoggedIn(): boolean {
+    debugger;
+    return this.googleApi.isLoggedIn()
+  }
+
+  logout() {
+    debugger;
+    this.googleApi.signOut()
+    debugger;
+  }
+
+  async getEmails() {
+    debugger;
+    if (!this.userInfo) {
+      debugger;
+      return;
+    }
+    debugger;
+    const userId = this.userInfo?.info.sub as string;
+    const messages = await lastValueFrom(this.googleApi.emails(userId));
+    debugger;
+    messages.messages.forEach( (element: any) => {
+      debugger;
+      const mail = lastValueFrom(this.googleApi.getMail(userId, element.id))
+      mail.then( mail => {
+        debugger;
+        this.mailSnippets.push(mail.snippet)
+        debugger;
+      })
+      debugger;
+    });
+    debugger;
+  }
 }
---
+++ src/app/token.interceptor.ts
@@ -0,0 +1,34 @@
+import { OAuthService } from 'angular-oauth2-oidc';
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+
+
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(private readonly oAuthService: OAuthService) {
+    debugger;
+  }
+
+  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+    debugger;
+    if (request.url.includes('openid-configuration')) {
+      return next.handle(request);
+      debugger;
+    }
+
+    const userToken = this.oAuthService.getAccessToken();
+    const modifiedReq = request.clone({
+      headers: request.headers.set('Authorization', `Bearer ${userToken}`),
+    });
+
+    return next.handle(modifiedReq);
+  }
+}
